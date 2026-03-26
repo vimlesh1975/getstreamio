9c9263373c779bdab561dd6eb895c36b5a577563
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2026-03-26 09:33:25 +0530

    titltle

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,12 @@
-"use client";
 
 import "@stream-io/video-react-sdk/dist/css/styles.css";
+
+import "./globals.css"; // 👈 This applies the CSS to every single page
+
+export const metadata = {
+  title: "Broadcast Studio",
+  description: "Real-time Guest Portal",
+};
 
 export default function RootLayout({ children }) {
   return (
